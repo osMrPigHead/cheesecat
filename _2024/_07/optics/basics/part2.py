03f11a7b0c45423038063cdc1cbf0391f7d2afb6
osMrPigHead <osmrpighead@outlook.com>
2024-08-15 21:27:33 +0800

    202407 光学释疑(optics)-basics部分动生电动势

--- _2024/_07/optics/basics/part2.py
+++ _2024/_07/optics/basics/part2.py
@@ -1,1 +1,138 @@
 """基础知识篇 Part 2: 电磁感应"""
+from _2024._07.optics.mobject import *
+from customs.utils import *
+from manimlib import *
+
+
+class DynamicEMF(Scene):
+    """tested with commit 656f98fd in osMrPighead/manimgl"""
+    def construct(self) -> None:
+        xxxxxx114514 = (VGroup(*(Text("+", font_size=20, color=BLUE_D)
+                               .rotate(PI/4)
+                               .move_to((x, y + 0.5, 0))
+                                 for x in range(-8, 8) for y in range(-5, 5))))
+        line = (Cylinder(radius=0.1, height=20)
+                .rotate(PI/2, RIGHT)
+                .move_to((-5, 0, 0))
+                .add_updater(lambda mob, dt: mob.shift((dt/4, 0, 0))))
+        b_tex = Tex(R"B", color=BLUE_D).to_edge(UL)
+        v_arrow = (Arrow(LEFT, RIGHT, stroke_color=YELLOW, buff=0)
+                   .add_updater(lambda mob: mob.put_start_and_end_on(
+                       line.get_center(),
+                       line.get_center() + (1, 0, 0)
+                   )))
+        v_tex = Tex(R"v", color=YELLOW).add_updater(lambda mob: mob.next_to(v_arrow, UP))
+        i_arrow = (Arrow(DOWN, UP, stroke_color=BLUE_D, buff=0)
+                   .add_updater(lambda mob: mob.next_to(line, LEFT)))
+        i_tex = Tex(R"I", color=BLUE_D).add_updater(lambda mob: mob.next_to(i_arrow, LEFT))
+        self.add(xxxxxx114514, line)
+        self.wait()
+        self.play(Write(i_arrow), Write(i_tex))
+        self.wait()
+        self.remove(line)
+        self.add(v_arrow, line)
+        self.play(FadeIn(BackgroundRectangle(b_tex)), Write(b_tex),
+                  Write(v_arrow), Write(v_tex))
+        self.wait()
+        e1 = (Charge(ORIGIN, -1, 0.1, 0)
+              .add_updater(lambda mob: mob.move_to(line.get_center() + (-0.02*line.get_center()[0], 0, 0.2))))
+        self.play(FadeIn(e1))
+        self.wait()
+        lorentz_force_arrow1 = (Arrow(LEFT, RIGHT, stroke_color=YELLOW, buff=0)
+                                .add_updater(lambda mob: mob.put_start_and_end_on(
+                                    e1.get_center(),
+                                    e1.get_center() - (0, 1.5, 0)
+                                )))
+        lorentz_force_tex1 = (Tex(R"-e\vec{v}\times\vec{B}", color=YELLOW)
+                              .scale(0.8)
+                              .add_updater(lambda mob: mob.next_to(e1.get_center() - (0, 1.5, 0), RIGHT)))
+        self.remove(e1)
+        self.add(lorentz_force_arrow1, e1)
+        self.play(Write(lorentz_force_arrow1), Write(lorentz_force_tex1))
+        self.wait()
+        e1.clear_updaters()
+        f = color_by_z([BLUE, RED], -6, 6)
+        self.play(UpdateFromAlphaFunc(
+            e1, lambda mob, alpha: mob.move_to(line.get_center() + (-0.02*line.get_center()[0], -8*alpha, 0.2))
+        ), line.animate.set_color_by_rgb_func(lambda p: f((p[0], p[2], p[1]))))
+        self.wait()
+        e_arrow = (Arrow(LEFT, RIGHT, stroke_color=GREEN, buff=0)
+                   .add_updater(lambda mob: mob.put_start_and_end_on(
+                       line.get_center() + (0, 0.5, 0),
+                       line.get_center() - (0, 0.5, 0)
+                   )))
+        e_tex = (Tex(R"\vec{E}", color=GREEN)
+                 .add_updater(lambda mob: mob.next_to(line.get_center() - (0, 0.5, 0), RIGHT)))
+        self.play(Write(e_arrow), Write(e_tex))
+        self.wait()
+        e2 = (Charge(ORIGIN, -1, 0.1, 0)
+              .add_updater(lambda mob: mob.move_to(line.get_center() + (-0.02*line.get_center()[0], 0, 0.2))))
+        lorentz_force_arrow2 = (Arrow(LEFT, RIGHT, stroke_color=YELLOW, buff=0)
+                                .add_updater(lambda mob: mob.put_start_and_end_on(
+                                    e2.get_center(),
+                                    e2.get_center() - (0, 1.5, 0)
+                                )))
+        lorentz_force_tex2 = (Tex(R"-e\vec{v}\times\vec{B}", color=YELLOW)
+                              .scale(0.8)
+                              .add_updater(lambda mob: mob.next_to(e2.get_center() - (0, 1.5, 0), RIGHT)))
+        self.play(*(FadeIn(mob) for mob in [lorentz_force_arrow2, lorentz_force_tex2, e2]))
+        e_force_arrow = Arrow(LEFT, RIGHT, stroke_color=GOLD, buff=0)
+        e_force_tex = (Tex(R"-e\vec{E}", color=GOLD)
+                       .scale(0.8)
+                       .add_updater(lambda mob: mob.next_to(e_force_arrow.get_end(), RIGHT)))
+        self.remove(e2)
+        self.add(e_force_arrow, e2)
+        self.play(UpdateFromAlphaFunc(e_force_arrow, lambda mob, alpha: mob.put_start_and_end_on(
+            e2.get_center(),
+            e2.get_center() + (0, 1.5*smooth(alpha), 0)
+        ), rate_func=linear, run_time=2), Write(e_force_tex, suspend_mobject_updating=False),
+                  *(FadeOut(mob, time_span=(1, 2)) for mob in [i_arrow, i_tex]))
+        e_force_arrow.add_updater(lambda mob: mob.put_start_and_end_on(
+                                      e2.get_center(),
+                                      e2.get_center() + (0, 1.5, 0)
+                                  ))
+        self.wait()
+        line_up = Line(LEFT, RIGHT, stroke_color=YELLOW).add_updater(lambda mob: mob.put_start_and_end_on(
+            line.get_center() + (0, 3, 0),
+            line.get_center() + (-2, 3, 0)
+        ))
+        line_down = Line(LEFT, RIGHT, stroke_color=YELLOW).add_updater(lambda mob: mob.put_start_and_end_on(
+            line.get_center() + (0, -3, 0),
+            line.get_center() + (-2, -3, 0)
+        ))
+        arrow_up = Arrow(LEFT, RIGHT, stroke_color=YELLOW).add_updater(lambda mob: mob.put_start_and_end_on(
+            line.get_center() + (-1.5, 0.5, 0),
+            line.get_center() + (-1.5, 3, 0)
+        ))
+        arrow_down = Arrow(LEFT, RIGHT, stroke_color=YELLOW).add_updater(lambda mob: mob.put_start_and_end_on(
+            line.get_center() + (-1.5, -0.5, 0),
+            line.get_center() + (-1.5, -3, 0)
+        ))
+        u_sign = (Tex(R"U", color=YELLOW)
+                  .add_updater(lambda mob: mob.move_to(line.get_center() + (-1.5, 0, 0))))
+        self.play(*(Write(mob) for mob in [line_up, line_down, arrow_up, arrow_down, u_sign]))
+        self.wait()
+        ek_arrow = (Arrow(LEFT, RIGHT, stroke_color=GOLD, buff=0)
+                    .add_updater(lambda mob: mob.put_start_and_end_on(
+                        line.get_center() + (1.5, -0.5, 0),
+                        line.get_center() + (1.5, 0.5, 0)
+                    )))
+        ek_tex = (Tex(R"\vec{E}_k", color=GOLD)
+                  .add_updater(lambda mob: mob.next_to(ek_arrow, RIGHT)))
+        self.play(Write(ek_arrow), Write(ek_tex))
+        self.wait()
+        self.play(Transform(lorentz_force_tex2, Tex(R"-e\vec{E}_k", color=YELLOW)
+                            .add_updater(lambda mob: mob.next_to(e2.get_center() - (0, 1.5, 0), RIGHT))))
+        self.wait()
+        title = (Text("动生电动势", font_size=56)
+                 .to_edge(UP))
+        self.play(FadeIn(BackgroundRectangle(title)), Write(title))
+        self.wait()
+
+
+class DynamicEMFFormula(Scene):
+    """tested with commit 259640f5 in osMrPighead/manimgl"""
+    def construct(self) -> None:
+        dynamic_emf = Tex(R"\mathscr{E}=-Blv").to_edge(DL)
+        self.play(FadeIn(BackgroundRectangle(dynamic_emf)), Write(dynamic_emf))
+        self.wait()
